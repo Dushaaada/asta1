f9bdd9c097fd3146c6b5abb47ea3caa8ee3fc01a
Vlada <132074001+Dushaaada@users.noreply.github.com>
2023-05-22 23:05:51 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+  });
